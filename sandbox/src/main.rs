785c673bf1f9dae58e11d9ab33386cf3a0b7a52c
bjoernp116 <bjoernpollen@gmail.com>
2024-04-09 18:44:16 +0200

    window & event module

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,16 +3,5 @@ use nectar::entity::{self, new_entity};
 use nectar::archetype::{new_component, Archetype, ComponentID};
 fn main() {
     let mut world: Scene = Scene::new();
-    let player: entity::EntityID = new_entity();
-    let position: ComponentID = new_component();
-    nectar::run();
-
-    world.add_entity(player, Archetype::new());
-    entity::add_component(player, position, &mut world);
-    dbg!(&world.entity_index);
-    dbg!(&world.archetype_index);
-    dbg!(&world.component_index);
-    assert_eq!(entity::has_component(player, position, &world).unwrap(), true);
-
-
+    nectar::eventloop::run();
 }
