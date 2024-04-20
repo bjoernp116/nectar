61559721b7fb6b8555d3d8c3abc59a4a14f1fbb7
bjoernp116 <bjoernpollen@gmail.com>
2024-04-20 17:48:28 +0200

    Not Much

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,11 +1,10 @@
 use nectar::world::World;
-use nectar::defaults::{Transform3D, Renderer};
+use nectar::defaults::Transform3D;
 fn main() {
-    nectar::eventloop::run();
+
     let mut w: World = World::new();
     let entity = w.new_entity();
     w.add_component::<Transform3D>(entity, Transform3D::new());
     //w.add_component::<Renderer>(entity, Renderer::new());
-    let mut transform = w.get_component::<Transform3D>(entity).unwrap();
-
+    let mut _transform = w.get_component::<Transform3D>(entity).unwrap();
 }
